ece60e71ee938cc528fefda8d61e9868617a797a
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-09-02 17:28:20 +0700

    loggin env

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -13,7 +13,7 @@ export default function Register() {
 
   async function onSubmit(e: IRegisterForm){
     try{
-      console.log(import.meta.env.BASE_URL)
+      console.log(import.meta.env.VITE_API_BASE_URL)
       const results = await api.post('/auth/register', e)
       console.log(results)
       reset()
